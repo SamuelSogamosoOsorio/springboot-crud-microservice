598b147546c3e84164fd83654b5338440dde80ae
SamuelSogamosoOsorio <samuelso0512@gmail.com>
2025-04-07 19:49:03 -0500

    Proyecto base Spring Boot generado con Spring Initializr

---
+++ crud-microservice/src/test/java/com/example/crud_microservice/CrudMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crud_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
